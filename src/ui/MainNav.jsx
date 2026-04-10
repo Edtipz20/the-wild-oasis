49e9c94fed3429da349112fc932eafc18d248a95
Eduard <eduardo.tipoh@gmail.com>
2026-04-11 06:25:18 +0800

    fix: DarkModeToggle, Header, MainNav

--- src/ui/MainNav.jsx
+++ src/ui/MainNav.jsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import {
   HiOutlineCalendarDays,
   HiOutlineCog6Tooth,
-  HiOutlineHome,
   HiOutlineHomeModern,
   HiOutlineRectangleGroup,
   HiOutlineUsers,
